0fbc2cf3015776a2f5da1727f5c3d34b5feed6b3
Zani <zanoin@gmail.com>
2019-09-27 16:49:05 +0200

    1. removed some boilerplate code that was necessary for the cardViews 2. some costomization to the cardView 3. changed the theme to materaildesign

--- app/src/main/java/com/zanoApps/orderapp/LocalDB/CustomerEntity.kt
+++ app/src/main/java/com/zanoApps/orderapp/LocalDB/CustomerEntity.kt
@@ -9,7 +9,7 @@ data class CustomerEntity (
 
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "customer_id")
-    private val customerId: String?,
+    private val customerId: String,
     @ColumnInfo(name = "customer_name") private val customerName: String?,
     @ColumnInfo(name = "customer_number") private val customerNumber: String?,
     @ColumnInfo(name = "customer_address") private val customerAddress: String?
