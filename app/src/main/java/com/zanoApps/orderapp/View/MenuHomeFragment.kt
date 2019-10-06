ac52207a5a985160391fd1289922c99964666e4d
Zano <kranje_z@live.com>
2019-10-06 08:15:49 +0200

    added some animation to the menu fragment(on the cardViews ,buttons ,textView when on initialization) , also created some custom anim resource files

--- app/src/main/java/com/zanoApps/orderapp/View/MenuHomeFragment.kt
+++ app/src/main/java/com/zanoApps/orderapp/View/MenuHomeFragment.kt
@@ -6,10 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.card.MaterialCardView
 
 import com.zanoApps.orderapp.R
+import kotlinx.android.synthetic.main.fragment_menu_home.*
 
 /**
  * A simple [Fragment] subclass.
@@ -26,6 +30,8 @@ class MenuHomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        animateElementsInsideTheLayout()
+
 
         val moreOnDrinks = view.findViewById<Button>(R.id.moreDrinks)
         moreOnDrinks.setOnClickListener {
@@ -37,5 +43,33 @@ class MenuHomeFragment : Fragment() {
             findNavController().navigate(R.id.action_homeMenu_dest_to_saladCategory_dest)
         }
 
+    }
+
+    private fun animateElementsInsideTheLayout(){
+        /*declare the animations*/
+        val translateTopToBottom = AnimationUtils.loadAnimation(context, R.anim.top_into_bottom)
+        val scaleIn = AnimationUtils.loadAnimation(context, R.anim.scale_in)
+        val tBottomToTopShortDelay = AnimationUtils.loadAnimation(context, R.anim.translate_bottom_top_short_delay)
+        val tBottomToTopMediumDelay = AnimationUtils.loadAnimation(context, R.anim.translate_bottom_top_medium_delay)
+        val tBottomToTopLongDelay = AnimationUtils.loadAnimation(context, R.anim.translate_bottom_top_long_delay)
+
+        /*set the animation*/
+        card1.startAnimation(tBottomToTopShortDelay)
+        card2.startAnimation(tBottomToTopMediumDelay)
+        card3.startAnimation(tBottomToTopLongDelay)
+        card4.startAnimation(tBottomToTopShortDelay)
+        card5.startAnimation(tBottomToTopMediumDelay)
+        card6.startAnimation(tBottomToTopLongDelay)
+        card7.startAnimation(tBottomToTopShortDelay)
+        card8.startAnimation(tBottomToTopMediumDelay)
+        card9.startAnimation(tBottomToTopLongDelay)
+        moreDrinks.startAnimation(scaleIn)
+        moreSalads.startAnimation(scaleIn)
+        moreChicken.startAnimation(scaleIn)
+        moreMeat.startAnimation(scaleIn)
+        drinks_text_view.startAnimation(translateTopToBottom)
+        salad_text_view.startAnimation(translateTopToBottom)
+        chicken_text_view.startAnimation(translateTopToBottom)
+        meat_text_view.startAnimation(translateTopToBottom)
     }
 }
